9b5123b70a916d87677bbfd579a92e22ef09404a
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-26 21:04:26 +0530

    First Commit

---
+++ app/src/main/java/com/example/retrofit_jetpackcompose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.retrofit_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.retrofit_jetpackcompose.ui.theme.RetrofitJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RetrofitJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
